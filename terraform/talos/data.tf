5328f7f7ac98f988a79366698c499ea86bc2a409
Khiem Nguyen Dinh <khiem.nd@namitech.io>
2026-09-05 23:03:30 +0700

    update vault

--- terraform/talos/data.tf
+++ terraform/talos/data.tf
@@ -1,8 +1,3 @@
-data "vault_kv_secret_v2" "chartmuseum" {
-  mount = "secret"
-  name  = "platform/chartmuseum"
-}
-
 data "vault_kv_secret_v2" "talos" {
   mount = "secret"
   name  = "terraform/talos"
