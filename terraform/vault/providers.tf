5d789e9708c789aba3e75b95e1616b228c3520b3
Khiem Nguyen Dinh <khiem.nd@namitech.io>
2026-09-06 12:01:04 +0700

    vault internal

--- terraform/vault/providers.tf
+++ terraform/vault/providers.tf
@@ -1,4 +1,5 @@
 provider "vault" {
-  address = var.vault_address
-  token   = var.vault_token
+  address         = var.vault_address
+  token           = var.vault_token
+  skip_tls_verify = true
 }
