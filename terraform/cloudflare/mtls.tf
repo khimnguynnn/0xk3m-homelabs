9941722113dcaab242818a14d96679dfa34fec59
Khiem Nguyen Dinh <khiem.nd@namitech.io>
2026-09-06 21:41:55 +0700

    feat(cloudflare): add WAF rule to enforce mTLS on grafana.0xk3m.dev

--- terraform/cloudflare/mtls.tf
+++ terraform/cloudflare/mtls.tf
@@ -8,3 +8,19 @@ resource "cloudflare_certificate_authorities_hostname_associations" "mtls" {
   zone_id   = data.cloudflare_zone.homelab.id
   hostnames = local.mtls_hostnames
 }
+
+resource "cloudflare_ruleset" "mtls_enforcement" {
+  zone_id     = data.cloudflare_zone.homelab.id
+  name        = "mTLS Enforcement"
+  phase       = "http_request_firewall_custom"
+  kind        = "zone"
+  description = "Require valid client certificate for mTLS-protected hostnames"
+  rules = [
+    {
+      description = "Block requests without valid client certificate"
+      expression  = "(http.host in {\"${join("\" \"", local.mtls_hostnames)}\"}) and not cf.tls_client_auth.cert_verified"
+      action      = "block"
+      enabled     = true
+    }
+  ]
+}
