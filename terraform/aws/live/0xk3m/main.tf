2d0788cd7858ee9fe9fd4359d1400b2a033e6ee1
Khiem Nguyen Dinh <khiem.nd@namitech.io>
2026-09-11 12:07:38 +0700

    add bucket for open-webui

--- terraform/aws/live/0xk3m/main.tf
+++ terraform/aws/live/0xk3m/main.tf
@@ -53,3 +53,57 @@ module "iam_user" {
     loki_s3 = aws_iam_policy.loki_s3.arn
   }
 }
+
+# Open WebUI S3 Storage
+module "open_webui_s3_bucket" {
+  source = "terraform-aws-modules/s3-bucket/aws"
+
+  bucket = "0xk3m-open-webui"
+  acl    = "private"
+
+  control_object_ownership = true
+  object_ownership         = "ObjectWriter"
+
+  versioning = {
+    enabled = true
+  }
+}
+
+data "aws_iam_policy_document" "open_webui_s3" {
+  statement {
+    sid    = "OpenWebUIAccess"
+    effect = "Allow"
+
+    actions = [
+      "s3:PutObject",
+      "s3:GetObject",
+      "s3:DeleteObject",
+      "s3:ListBucket",
+    ]
+
+    resources = [
+      module.open_webui_s3_bucket.s3_bucket_arn,
+      "${module.open_webui_s3_bucket.s3_bucket_arn}/*",
+    ]
+  }
+}
+
+resource "aws_iam_policy" "open_webui_s3" {
+  name        = "0xk3m-open-webui-s3-access"
+  description = "Access to the 0xk3m-open-webui bucket for Open WebUI"
+  policy      = data.aws_iam_policy_document.open_webui_s3.json
+}
+
+module "open_webui_iam_user" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-user"
+  version = "~> 6.0"
+
+  name = "0xk3m-open-webui"
+
+  create_login_profile = false
+  create_access_key    = true
+
+  policies = {
+    open_webui_s3 = aws_iam_policy.open_webui_s3.arn
+  }
+}
--- terraform/aws/live/0xk3m/outputs.tf
+++ terraform/aws/live/0xk3m/outputs.tf
@@ -8,3 +8,25 @@ output "loki_s3_secret_access_key" {
   value       = module.iam_user.access_key_secret
   sensitive   = true
 }
+
+# Open WebUI
+output "open_webui_s3_bucket_name" {
+  description = "S3 bucket name for Open WebUI"
+  value       = module.open_webui_s3_bucket.s3_bucket_id
+}
+
+output "open_webui_s3_region" {
+  description = "S3 bucket region for Open WebUI"
+  value       = module.open_webui_s3_bucket.s3_bucket_region
+}
+
+output "open_webui_s3_access_key_id" {
+  description = "Access key ID for the Open WebUI S3 user"
+  value       = module.open_webui_iam_user.access_key_id
+}
+
+output "open_webui_s3_secret_access_key" {
+  description = "Secret access key for the Open WebUI S3 user"
+  value       = module.open_webui_iam_user.access_key_secret
+  sensitive   = true
+}
